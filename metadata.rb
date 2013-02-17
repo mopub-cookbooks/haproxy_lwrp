daceaff19b94df2b6bff7d1a25560e28282aa46f
Scott M. Likens <scott@likens.us>
2013-02-16 22:01:47 -0800

    update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-name "haproxy"
-maintainer        "Opscode, Inc."
-maintainer_email  "cookbooks@opscode.com"
+name "haproxy-lwrp"
+maintainer        "Scott M. Likens"
+maintainer_email  "scott@likens.us"
 license           "Apache 2.0"
 description       "Installs and configures haproxy"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
@@ -8,5 +8,4 @@ version "2.0.0"
 supports 'ubuntu', "= 12.04"
 recipe "haproxy", "Installs and configures haproxy"
 
-depends "logrotate"
 depends "runit"
