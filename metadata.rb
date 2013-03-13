4909612cb1f3d8a19f1d062b46dc3242684f5170
Scott M. Likens <scott@likens.us>
2013-03-12 19:08:43 -0700

    bump to 2.0.3

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email  "scott@likens.us"
 license           "Apache 2.0"
 description       "Installs and configures haproxy"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "2.0.2"
+version "2.0.3"
 supports 'ubuntu', "= 12.04"
 depends "runit"
